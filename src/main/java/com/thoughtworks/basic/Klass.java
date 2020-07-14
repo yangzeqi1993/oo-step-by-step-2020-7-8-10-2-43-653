b7e1fcf2469ec9e4a6b869bb4131b6a7cf63f07c
yang <yangzq19931003@outlook.com>
2020-07-14 19:45:22 +0800

    create Klass and Student class

---
+++ src/main/java/com/thoughtworks/basic/Klass.java
@@ -0,0 +1,17 @@
+package com.thoughtworks.basic;
+
+public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
---
+++ src/main/java/com/thoughtworks/basic/Student.java
@@ -0,0 +1,22 @@
+package com.thoughtworks.basic;
+
+public class Student extends Person {
+
+    private Klass classNumber;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce (){
+        return introduce();
+    }
+
+    public Klass getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(Klass classNumber) {
+        this.classNumber = classNumber;
+    }
+}
