5297505bc2813254451d93cad80fd0fdee2944df
yang <yangzq19931003@outlook.com>
2020-07-14 19:26:55 +0800

    finish No.1: create Person Class

---
+++ src/main/java/com/thoughtworks/basic/Main.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.basic;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Tom",18);
+        System.out.println(person.introduce());
+    }
+}
---
+++ src/main/java/com/thoughtworks/basic/Person.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.basic;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce (){
+        return "My name is " + this.name + ". I am " + this.age + " years old." ;
+    }
+}
